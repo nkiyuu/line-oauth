72a3145b06527f80a2da7964d48f39af53114777
Yuki Nozaki <nq127main@gmail.com>
2018-05-09 08:47:50 +0900

    サーバーのモックは作った

--- server.js
+++ server.js
@@ -7,17 +7,35 @@ import { Server } from 'net';
 
 const httpServer = http.createServer();
 
+// HTMLを返す
+function renderHTML(res, html) {
+  const template = fs.readFile(`./template/${html}`, 'utf-8', (err, data) => {
+    res.writeHead(200, {
+      'conten-Type': 'text/html'
+    });
+
+    res.write(data);
+    res.end(`${html} has already sent to browser`);
+  })
+}
+
 httpServer.on('request', (req, res) => {
   const Response = {
-    "renderHTML": () => {
-      const template = fs.readFile('./template/index.html', 'utf-8', (err, data) => {
-        res.writeHead(200, {
-          'content-Type': 'text/html'
-        });
-
-        res.write(data);
-        res.end('HTML file has already sent to browser')
-      });
-    }
+    "renderIndex": renderHTML(res, 'index.html'),
+    "renderLogin": renderHtml(res, 'login.html'),
   },
+
+  const uri = uri.parse(req.url).pathname
+
+  switch (uri) {
+    case "/" :
+      Response.renderIndex;
+      return;
+    case "/login" :
+      Response.renderLogin;
+      return;
+  }
 })
+
+httpServer.listen(8080);
+console.log()
