8f61b75990198c4c79bb0c0f19a9aec0f63a94f5
Yuki Nozaki <nq127main@gmail.com>
2018-05-13 23:57:07 +0900

    lineの認証に飛べるようにはなった

--- server.js
+++ server.js
@@ -2,28 +2,37 @@ const http = require('http');
 const fs = require('fs');
 const server = http.createServer();
 
-server.on('request', function(req, res) {
+server.on('request', function (req, res) {
   switch (req.url) {
     case '/index':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      fs.readFile(`./templates/index.html`, 'utf-8', (err, data) =>{
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      fs.readFile(`./templates/index.html`, 'utf-8', (err, data) => {
         res.write(data);
         res.end();
-      })
+      });
       break;
+
     case '/login':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      fs.readFile(`./templates/login.html`, 'utf-8', (err, data) =>{
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      fs.readFile(`./templates/login.html`, 'utf-8', (err, data) => {
         res.write(data);
         res.end();
       })
       break;
+
     default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
       res.write("404 not found");
       res.end();
       break;
-    }
+  }
 });
+
 server.listen(8080);
 console.log("server listening ...");
