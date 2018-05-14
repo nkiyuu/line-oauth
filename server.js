e1dafb6a2f6173fc3824174c5b26b58a651662e4
Yuki Nozaki <nq127main@gmail.com>
2018-05-15 01:06:48 +0900

    oauthでユーザの情報取れるようになった

--- server.js
+++ server.js
@@ -1,38 +1,72 @@
-const http = require('http');
 const fs = require('fs');
-const server = http.createServer();
+const express = require('express')
+const request = require('request');
 
-server.on('request', function (req, res) {
-  switch (req.url) {
-    case '/index':
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      fs.readFile(`./templates/index.html`, 'utf-8', (err, data) => {
-        res.write(data);
-        res.end();
-      });
-      break;
+const CONFIG = require('./config.json');
+const app = express();
 
-    case '/login':
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      fs.readFile(`./templates/login.html`, 'utf-8', (err, data) => {
-        res.write(data);
-        res.end();
-      })
-      break;
+let accessToken = '';
 
-    default:
-      res.writeHead(404, {
-        'Content-Type': 'text/plain'
-      });
-      res.write("404 not found");
-      res.end();
-      break;
+app.get('/index', (req, res) => {
+  let displayName = '';
+  let pictureUrl = '';
+  let options = {
+    uri: 'https://api.line.me/v2/profile',
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
   }
+
+  request.get(options, function(error, response, body) {
+    console.log(body);
+    displayName = JSON.parse(body).displayName;
+    pictureUrl = JSON.parse(body).pictureUrl;
+    res.send(`
+      <!DOCTYPE html>
+      <html lang="ja">
+        <head>
+          <meta charset="UTF-8">
+          <title>HTML5サンプル</title>
+        </head>
+        <body>
+          <h1>プロフィール</h1>
+          <div>${displayName}</div>
+          <div><img src="${pictureUrl}"></div>
+        </body>
+      </html>
+    `);
+    res.end();
+  });
 });
 
-server.listen(8080);
-console.log("server listening ...");
+app.get('/login', (req, res) => {
+  fs.readFile(`./templates/login.html`, 'utf-8', (err, data) => {
+    res.send(data);
+    res.end();
+  });
+});
+
+app.get('/get_token', (req, res) => {
+  const code = req.query.code;
+
+  let options = {
+    uri: 'https://api.line.me/v2/oauth/accessToken',
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    form: {
+      'grant_type': 'authorization_code',
+      'code': code,
+      'client_id': CONFIG.CLIENT_ID,
+      'client_secret': CONFIG.CLIENT_SECRET,
+      'redirect_uri': 'http://localhost:8080/get_token',
+    }
+  }
+  request.post(options, function(error, response, body){
+    accessToken = JSON.parse(body).access_token
+
+    res.redirect('/index');
+  });
+})
+
+app.listen(8080, function () {});
