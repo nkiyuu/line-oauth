19dd01014d9ae90cdffb0f727288202d629d4112
Yuki Nozaki <nq127main@gmail.com>
2018-05-15 07:49:03 +0900

    server側でhtmlを書いてしまった

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.get('/index', (req, res) => {
     }
   }
 
-  request.get(options, function(error, response, body) {
-    console.log(body);
+  request.get(options, function (error, response, body) {
     displayName = JSON.parse(body).displayName;
     pictureUrl = JSON.parse(body).pictureUrl;
     res.send(`
@@ -29,8 +28,7 @@ app.get('/index', (req, res) => {
           <title>HTML5サンプル</title>
         </head>
         <body>
-          <h1>プロフィール</h1>
-          <div>${displayName}</div>
+          <h1>${displayName}のプロフィール画像</h1>
           <div><img src="${pictureUrl}"></div>
         </body>
       </html>
@@ -40,10 +38,22 @@ app.get('/index', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  fs.readFile(`./templates/login.html`, 'utf-8', (err, data) => {
-    res.send(data);
-    res.end();
-  });
+  res.send(`
+    <!DOCTYPE html>
+      <html lang="ja">
+      <head>
+        <meta charset="UTF-8">
+        <title>HTML5サンプル</title>
+      </head>
+      <body>
+        <h1>これはLogin</h1>
+
+        <div><a href="https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=${CONFIG.CLIENT_ID}&redirect_uri=${CONFIG.REDIRECT_URI}&state=fjdkpwenvaj4820fkldas">lineで認可してみる</a></div>
+      </body>
+
+    </html>
+  `)
+  res.end();
 });
 
 app.get('/get_token', (req, res) => {
@@ -62,11 +72,13 @@ app.get('/get_token', (req, res) => {
       'redirect_uri': 'http://localhost:8080/get_token',
     }
   }
-  request.post(options, function(error, response, body){
+  request.post(options, function (error, response, body) {
     accessToken = JSON.parse(body).access_token
 
     res.redirect('/index');
   });
 })
 
-app.listen(8080, function () {});
+app.listen(8080, function () {
+  console.log('start server ... ')
+});
